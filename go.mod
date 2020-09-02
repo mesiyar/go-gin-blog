50b04b317065c35d1862d1fdd54e2199a0836954
罗凯 <luokai@daishujiankang.com>
2020-09-02 17:49:52 +0800

    tags model 及api逻辑处理

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gin-blog
 go 1.15
 
 require (
+	github.com/astaxie/beego v1.12.2
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-ini/ini v1.60.2
 	github.com/jinzhu/gorm v1.9.16
--- src/gin-blog/models/tag.go
+++ src/gin-blog/models/tag.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
 type Tag struct {
 	Model
 
@@ -9,14 +14,66 @@ type Tag struct {
 	State      int    `json:"state"`
 }
 
+// 获取tag
 func GetTags(pageNum int, pageSize int, maps interface{}) (tags []Tag) {
 	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
 
 	return
 }
 
+// 获取总数
 func GetTagTotal(maps interface{}) (count int) {
 	db.Model(&Tag{}).Where(maps).Count(&count)
 
 	return
+}
+
+// 检查标签是否存在
+func ExistsByName(name string) bool {
+	var tag Tag
+	db.Select("id").Where("name = ?", name).First(&tag)
+	return tag.ID > 0
+}
+
+// 新增标签
+func AddTag(name string, state int, createdBy string) bool {
+	db.Create(&Tag{
+		Name:      name,
+		CreatedBy: createdBy,
+		State:     state,
+	})
+	return true
+}
+// 回调 创建
+func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("CreatedOn", time.Now().Unix())
+	return nil
+}
+
+// 回调 更新
+func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+	return nil
+}
+
+func ExistTagByID(id int) bool {
+	var tag Tag
+	db.Select("id").Where("id = ?", id).First(&tag)
+	if tag.ID > 0 {
+		return true
+	}
+
+	return false
+}
+
+func EditTag(id int, data interface {}) bool {
+	db.Model(&Tag{}).Where("id = ?", id).Updates(data)
+
+	return true
+}
+
+func DeleteTag(id int) bool {
+	db.Where("id = ?", id).Delete(&Tag{})
+
+	return true
 }
